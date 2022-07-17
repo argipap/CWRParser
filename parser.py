2d99a2b01c05c744997e8c9d684fd6632dac0e5b
argipap <argiris.papaefstathiou@orfium.com>
2022-07-17 14:48:19 +0300

    refactor

--- parser.py
+++ parser.py
@@ -2,12 +2,14 @@ import codecs
 import csv
 import re
 import sys
+from typing import Dict
+
 from tqdm import tqdm
 
 
-def find_asset_occurences(initial_dictionary, delimeter=";"):
+def find_asset_occurences(asset_blocks: Dict, delimeter=";") -> Dict:
     occurences = dict()
-    items = initial_dictionary.items()
+    items = asset_blocks.items()
     with tqdm(
         desc="Find duplicate assets ...",
         unit="assets",
@@ -15,7 +17,7 @@ def find_asset_occurences(initial_dictionary, delimeter=";"):
     ) as bar:
         for _, value in items:
             asset_title = value[0]
-            writers = re.sub("\s+"," ", ",".join(value[1:])) if len(value)>1 else ""
+            writers = re.sub("\s+", " ", ",".join(value[1:])) if len(value) > 1 else ""
             asset_record_key = f"{asset_title}{delimeter}{writers}"
             if not occurences.get(asset_record_key):
                 occurences[asset_record_key] = 1
@@ -25,8 +27,9 @@ def find_asset_occurences(initial_dictionary, delimeter=";"):
     return occurences
 
 
-def write_unique(occurences):
-    output_file = "unique_assets_results.csv"
+def write_unique(
+    occurences: Dict, output_file: str = "unique_assets_results.csv"
+) -> None:
     with open(output_file, "w") as csv_file:
         total_occurences = occurences.items()
         with tqdm(
@@ -45,7 +48,9 @@ def write_unique(occurences):
                 bar.update(1)
 
 
-def write_duplicates(occurences):
+def write_duplicates(
+    occurences: Dict, output_file: str = "duplicate_assets_results.csv"
+) -> None:
     output_file = "duplicate_assets_results.csv"
     with open(output_file, "w") as csv_file:
         total_occurences = occurences.items()
@@ -66,12 +71,11 @@ def write_duplicates(occurences):
                 bar.update(1)
 
 
-def main():
+def parse_cwr_file(cwr_filename: str) -> Dict:
+    asset_writer_dictionary = {}
+    asset_index_key = 0
 
-    cwr_filename = sys.argv[1]
     with codecs.open(cwr_filename, encoding="latin-1") as cwr_file:
-        asset_writer_dictionary = {}
-        counter = 0
         total_lines = cwr_file.readlines()
         with tqdm(
             desc="Parsing CWR File Line by Line ...",
@@ -79,24 +83,25 @@ def main():
             total=len(total_lines),
         ) as bar:
             for line in total_lines:
-                if line[:3] in ["NWR", "REV", "EXC", "ISW"]:
-                    counter +=1
+                if line.startswith(("NWR", "REV", "EXC", "ISW")):
+                    asset_index_key += 1
                     asset_title = line[19:80].strip()
-                    asset_writer_dictionary[counter] = [asset_title]
-                elif line[:3] in ["SWR", "OWR"]:
+                    asset_writer_dictionary[asset_index_key] = [asset_title]
+                if line.startswith(("SWR", "OWR")):
                     writer = line[28:104].strip()
-                    existing_value = asset_writer_dictionary.get(counter)
+                    existing_value = asset_writer_dictionary.get(asset_index_key)
                     if existing_value:
                         existing_value.append(writer)
-                        asset_writer_dictionary[counter] = existing_value
-                else:
-                    pass
+                        asset_writer_dictionary[asset_index_key] = existing_value
                 bar.update(1)
+    return asset_writer_dictionary
 
 
-        
-        occurences = find_asset_occurences(asset_writer_dictionary)
+def main():
 
+    cwr_filename = sys.argv[1]
+    asset_blocks = parse_cwr_file(cwr_filename)
+    occurences = find_asset_occurences(asset_blocks)
     write_unique(occurences)
     write_duplicates(occurences)
 
